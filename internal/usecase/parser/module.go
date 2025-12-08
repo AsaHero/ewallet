e3f9efb221e267531861e8bf82bb11c72068222a
asad <asadbahtiyarov2002@gmail.com>
2025-12-08 14:25:46 +0500

    time extending

--- internal/usecase/parser/module.go
+++ internal/usecase/parser/module.go
@@ -17,12 +17,12 @@ type Module struct {
 	Command Command
 }
 
-func NewModule(timeout time.Duration, logger *logger.Logger, llmClient ports.LLMProvider, ocrProvider ports.OCRProvider) *Module {
+func NewModule(logger *logger.Logger, llmClient ports.LLMProvider, ocrProvider ports.OCRProvider) *Module {
 	return &Module{
 		Command: Command{
-			parseTextUsecase:  NewParseTextUsecase(timeout, logger, llmClient),
-			parseAudioUsecase: NewParseAudioUsecase(timeout, logger, llmClient),
-			parseImageUsecase: NewParseImageUsecase(timeout, logger, llmClient, ocrProvider),
+			parseTextUsecase:  NewParseTextUsecase(1*time.Minute, logger, llmClient),
+			parseAudioUsecase: NewParseAudioUsecase(1*time.Minute, logger, llmClient),
+			parseImageUsecase: NewParseImageUsecase(1*time.Minute, logger, llmClient, ocrProvider),
 		},
 	}
 }
